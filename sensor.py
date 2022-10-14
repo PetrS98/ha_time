8002a72051253ba8332bc4af7de8f6399ce809af
Petr Staněk <aapetr12@email.cz>
2022-10-14 15:31:59 +0200

    .

--- sensor.py
+++ sensor.py
@@ -1,4 +1,5 @@
 """Platform for sensor integration."""
+from sqlite3 import Date
 import voluptuous as vol
 from homeassistant.components.sensor import PLATFORM_SCHEMA
 import homeassistant.helpers.config_validation as cv
@@ -6,25 +7,55 @@ from homeassistant.components.sensor import SensorEntity
 
 """ External Imports """
 import logging
-import requests
 import datetime
+
+""" Constants """
+CONF_SETTINGS = "settings"
 
 _LOGGER = logging.getLogger(__name__)
 
+PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(
+    {
+        vol.Required(CONF_SETTINGS): cv.positive_int
+    }
+)
+
 def setup_platform(hass, config, add_entities, discovery_info=None):
     """Set up the sensor platform."""
+    MainValNumber = config.get(CONF_SETTINGS)
 
-    add_entities([CustomTime()], update_before_add=True)
+    add_entities([CustomTime(MainValNumber)], update_before_add=True)
 
 class CustomTime(SensorEntity):
 
     """Representation of a Sensor."""
 
-    def __init__(self):
+    CZMonthName = {
+                1: "ledna",
+                2: "února",
+                3: "března",
+                4: "dubna",
+                5: "května",
+                6: "června",
+                7: "července",
+                8: "srpna",
+                9: "září",
+                10: "října",
+                11: "listopadu",
+                12: "prosince"
+            }
+    Strings = []
+
+    def __init__(self, MainValNumber):
         """Initialize the sensor."""
 
-        self._value = None
         self._available = None
+
+        if MainValNumber < 0 or MainValNumber > 4:
+            self._mainValNumber = 0
+        else:
+            self._mainValNumbe = MainValNumber
+         
 
     @property
     def unique_id(self):
@@ -39,8 +70,8 @@ class CustomTime(SensorEntity):
     @property
     def native_value(self):
         """Return the native value of the sensor."""
-        return self._value
-
+        return self.Strings[self._mainValNumber]
+       
     @property
     def available(self):
         """Return True if entity is available."""
@@ -52,7 +83,13 @@ class CustomTime(SensorEntity):
         This is the only method that should fetch new data for Home Assistant.
         """
         try:
-            self._value = datetime.datetime.now()
+            ActualDateTime = datetime.datetime.now()
+
+            self.Strings.append(ActualDateTime.strftime("%d.%m.%Y %H:%M:%S"))
+            self.Strings.append(ActualDateTime.strftime("%d." + self.CZMonthName[ActualDateTime.month] + " %Y %H:%M:%S"))
+            self.Strings.append(ActualDateTime.strftime("%d.%m.%Y"))
+            self.Strings.append(ActualDateTime.strftime("%d." + self.CZMonthName[ActualDateTime.month] + " %Y"))
+            self.Strings.append(ActualDateTime.strftime("%H:%M:%S"))
             self._available = True
         except:
             _LOGGER.exception("Error occured.")
